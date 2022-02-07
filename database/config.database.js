58c207d0146e37764517706bcbe518058c9227f2
francesco1702 <dfsaldanar@unc.edu.pe>
2022-02-07 00:16:21 -0500

    sequelize conexion

--- database/config.database.js
+++ database/config.database.js
@@ -1,7 +1,8 @@
 const Sequelize = require("sequelize");
 const { env } = require("../config/enviroments");
 if (process.env.NODE_ENV == "production") {
-    module.exports.sequelize = new Sequelize(process.env.DATABASE_URL);
+    const sequelize = new Sequelize(process.env.DATABASE_URL);
+    module.exports = sequelize;
 } else {
     const sequelize = new Sequelize(env.DB_NAME, env.DB_USER, env.DB_PASS, {
         host: env.DB_HOST,
