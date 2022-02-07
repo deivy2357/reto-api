91f44bd1adc77f2e3c863bf1a9b2fe6ee91ad5d2
francesco1702 <dfsaldanar@unc.edu.pe>
2022-02-07 00:21:39 -0500

    cambios ssl

--- database/config.database.js
+++ database/config.database.js
@@ -1,7 +1,16 @@
 const Sequelize = require("sequelize");
 const { env } = require("../config/enviroments");
 if (process.env.NODE_ENV == "production") {
-    const sequelize = new Sequelize(process.env.DATABASE_URL);
+    // const sequelize = new Sequelize(process.env.DATABASE_URL );
+    // module.exports = sequelize;
+    const sequelize = new Sequelize(env.DB_NAME, env.DB_USER, env.DB_PASS, {
+        host: env.DB_HOST,
+        port: env.DB_PORT,
+        dialect: "postgres",
+        dialectOptions: {
+            ssl: true,
+        },
+    });
     module.exports = sequelize;
 } else {
     const sequelize = new Sequelize(env.DB_NAME, env.DB_USER, env.DB_PASS, {
