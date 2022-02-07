207f44aac3c4b125768fd41cf16ec737bb7f442d
francesco1702 <dfsaldanar@unc.edu.pe>
2022-02-07 00:07:54 -0500

    db production

--- database/config.database.js
+++ database/config.database.js
@@ -1,10 +1,12 @@
 const Sequelize = require("sequelize");
 const { env } = require("../config/enviroments");
-
-const sequelize = new Sequelize(env.DB_NAME, env.DB_USER, env.DB_PASS, {
-    host: env.DB_HOST,
-    port: env.DB_PORT,
-    dialect: "postgres",
-});
-
-module.exports = sequelize;
+if (process.env.NODE_ENV == "production") {
+    module.exports.sequelize = new Sequelize(process.env.DATABASE_URL);
+} else {
+    const sequelize = new Sequelize(env.DB_NAME, env.DB_USER, env.DB_PASS, {
+        host: env.DB_HOST,
+        port: env.DB_PORT,
+        dialect: "postgres",
+    });
+    module.exports = sequelize;
+}
